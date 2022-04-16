cb0e20aca05bbd1afb318cceceea1f9b87558b45
gchattre@ur.rochester.edu <gchattre@ur.rochester.edu>
2022-04-16 03:00:52 +0000

    This is the Assignment 6 first push

---
+++ project3-mlops (2)/Labs/04-Lab-Runnable.py
@@ -0,0 +1,118 @@
+# Databricks notebook source
+# TODO
+dbutils.widgets.text("n_estimators", "100")
+ 
+dbutils.widgets.text("learning_rate", ".1")
+ 
+dbutils.widgets.text("max_depth", "1")
+ 
+import os
+ 
+import mlflow
+ 
+from mlflow.tracking import MlflowClient
+ 
+ 
+ 
+client = MlflowClient()
+
+# COMMAND ----------
+
+# TODO
+n_estimators = int(dbutils.widgets.get("n_estimators")) 
+learning_rate = float(dbutils.widgets.get("learning_rate"))  
+max_depth = int(dbutils.widgets.get("max_depth"))
+
+# COMMAND ----------
+
+# TODO
+#Train and log the results from a model.  Try using Gradient Boosted Trees
+#https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.GradientBoostingRegressor.html#sklearn.ensemble.GradientBoostingRegressor
+
+import mlflow
+ 
+import mlflow.sklearn
+ 
+import pandas as pd
+ 
+from sklearn.model_selection import train_test_split
+ 
+from sklearn.ensemble import GradientBoostingRegressor
+ 
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+ 
+ 
+ 
+with mlflow.start_run() as run:
+ 
+  # Import the data
+ 
+  df = pd.read_csv("/dbfs/mnt/training/airbnb/sf-listings/airbnb-cleaned-mlflow.csv")
+ 
+  X_train, X_test, y_train, y_test = train_test_split(df.drop(["price"], axis=1), df[["price"]].values.ravel(), random_state=42)
+ 
+    
+ 
+  # Create model, train it, and create predictions
+ 
+  rf = GradientBoostingRegressor(n_estimators=n_estimators, max_depth=max_depth, learning_rate=learning_rate)
+ 
+  rf.fit(X_train, y_train)
+ 
+  predictions = rf.predict(X_test)
+ 
+ 
+ 
+  # Log model
+ 
+  model_path = "gradient-boost-model"
+ 
+  mlflow.sklearn.log_model(rf, model_path)
+ 
+    
+ 
+  # Log params
+ 
+  mlflow.log_param("n_estimators", n_estimators)
+ 
+  mlflow.log_param("max_depth", max_depth)
+ 
+  mlflow.log_param("learning_rate", learning_rate)
+ 
+ 
+ 
+  # Log metrics
+ 
+  mlflow.log_metric("mse", mean_squared_error(y_test, predictions))
+ 
+  mlflow.log_metric("mae", mean_absolute_error(y_test, predictions))  
+ 
+  mlflow.log_metric("r2", r2_score(y_test, predictions)) 
+ 
+  model_output_path = "runs:/" + run.info.run_id + "/" + model_path
+
+    
+
+# COMMAND ----------
+
+# # TODO
+# Report the model output path to the parent notebook
+import json
+dbutils.notebook.exit(json.dumps({
+  "status": "OK",
+  "run_id":'5a5eb0dfb53e455bbfaabc1f0bac2384',
+  
+}))
+
+
+# COMMAND ----------
+
+# MAGIC %md-sandbox
+# MAGIC &copy; 2020 Databricks, Inc. All rights reserved.<br/>
+# MAGIC Apache, Apache Spark, Spark and the Spark logo are trademarks of the <a href="http://www.apache.org/">Apache Software Foundation</a>.<br/>
+# MAGIC <br/>
+# MAGIC <a href="https://databricks.com/privacy-policy">Privacy Policy</a> | <a href="https://databricks.com/terms-of-use">Terms of Use</a> | <a href="http://help.databricks.com/">Support</a>
+
+# COMMAND ----------
+
+
